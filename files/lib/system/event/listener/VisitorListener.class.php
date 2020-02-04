0e9a05bab58b6a2a576ec029387a75b766cccbbe
Matthias Kittsteiner <mk@kittmedia.com>
2020-02-04 19:27:12 +0100

    Add storing of page title and host, improve removing URL params

--- files/lib/system/event/listener/VisitorListener.class.php
+++ files/lib/system/event/listener/VisitorListener.class.php
@@ -3,10 +3,12 @@ namespace wcf\system\event\listener;
 use wcf\data\visitor\Visitor;
 use wcf\data\visitor\VisitorAction;
 use \wcf\system\WCF;
-use function strpos;
-use function substr;
+use wcf\util\StringUtil;
+use function explode;
+use function implode;
+use function parse_url;
+use function preg_replace;
 use const TIME_NOW;
-use const URL_OMIT_INDEX_PHP;
 
 /**
  * Add new user visits.
@@ -17,15 +19,35 @@ use const URL_OMIT_INDEX_PHP;
  * @package	com.kittmedia.wcf.visitors
  */
 class VisitorListener implements IParameterizedEventListener {
+	const REGEX_FILTER_HTML = '/\<\w[^<>]*?\>([^<>]+?\<\/\w+?\>)?|\<\/\w+?\>/';
+	
 	/**
 	 * @inheritDoc
 	 */
 	public function execute($eventObj, $className, $eventName, array &$parameters) {
 		if (Visitor::skipTracking()) return;
 		
+		// get title
+		$title = preg_replace(self::REGEX_FILTER_HTML, '', WCF::getTPL()->get('contentTitle'));
+		
+		if (!$title) {
+			$title = preg_replace(self::REGEX_FILTER_HTML, '', WCF::getTPL()->get('pageTitle'));
+		}
+		
+		// get host
+		if (WCF::getActivePath() !== null) {
+			$urlParts = parse_url(WCF::getActivePath());
+			$host = $urlParts['scheme'] . '://' . $urlParts['host'];
+		}
+		else {
+			$host = WCF::getActiveApplication()->domainName;
+		}
+		
 		(new VisitorAction([], 'create', [
 			'data' => [
-				'requestUri' => $this->removeQueryParameters(WCF::getSession()->requestURI),
+				'requestUri' => $this->removeQueryParameters($_SERVER['REQUEST_URI']),
+				'title' => $title,
+				'host' => $host,
 				'isRegistered' => WCF::getSession()->userID ? 1 : 0,
 				'time' => TIME_NOW
 			]
@@ -39,17 +61,22 @@ class VisitorListener implements IParameterizedEventListener {
 	 * @return	string
 	 */
 	private function removeQueryParameters($requestUri) {
-		$delimiter = '?';
+		$parts = explode('&', $requestUri);
 		
-		if (!URL_OMIT_INDEX_PHP) {
-			$delimiter = '&';
+		foreach ($parts as $key => &$part) {
+			if (
+				StringUtil::startsWith($part, 's=')
+				|| StringUtil::startsWith($part, '?s=')
+				|| StringUtil::startsWith($part, 't=')
+				|| StringUtil::startsWith($part, '?t=')
+			) {
+				unset($parts[$key]);
+			}
+			
+			$part = preg_replace('/(\?|&)(s|t)\=([^&?]+)/', '', $part);
 		}
 		
-		if (!strpos($requestUri, $delimiter)) {
-			return $requestUri;
-		}
-		
-		return (string) substr($requestUri, 0, strpos($requestUri, $delimiter));
+		return implode('&', $parts);
 	}
 	
 }
--- install.sql
+++ install.sql
@@ -2,6 +2,8 @@ DROP TABLE wcf1_visitor;
 CREATE TABLE wcf1_visitor (
 	visitorID	INT(10)			NOT NULL	AUTO_INCREMENT	PRIMARY KEY,
 	requestUri	VARCHAR(255)		NOT NULL,
+	title		VARCHAR(255)		NOT NULL,
+	host		VARCHAR(255)		NOT NULL,
 	isRegistered	TINYINT(1)		NOT NULL DEFAULT 0,
 	time		INT(10)			NOT NULL
 );
