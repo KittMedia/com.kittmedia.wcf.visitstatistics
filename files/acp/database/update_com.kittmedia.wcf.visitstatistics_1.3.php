c148d8d3e6962081fae228dcb8d659ae54e37d6e
Matthias Kittsteiner <mk@kittmedia.com>
2023-05-20 10:23:02 +0200

    Make sure to create named indizes

--- files/acp/database/update_com.kittmedia.wcf.visitstatistics_1.3.php
+++ files/acp/database/update_com.kittmedia.wcf.visitstatistics_1.3.php
@@ -1,44 +1,49 @@
 <?php
-use wcf\system\database\table\column\DateDatabaseTableColumn;
-use wcf\system\database\table\column\FloatDatabaseTableColumn;
-use wcf\system\database\table\column\IntDatabaseTableColumn;
-use wcf\system\database\table\column\NotNullVarchar255DatabaseTableColumn;
-use wcf\system\database\table\column\ObjectIdDatabaseTableColumn;
-use wcf\system\database\table\column\TinyintDatabaseTableColumn;
-use wcf\system\database\table\DatabaseTable;
-use wcf\system\database\table\PartialDatabaseTable;
 
 /**
- * Updates the database layout during the update from 1.2 to 1.3.
- * 
- * @author	Matthias Kittsteiner
- * @copyright	2022 KittMedia
- * @license	Free <https://shop.kittmedia.com/core/licenses/#licenseFree>
- * @package	com.kittmedia.wcf.visitstatistics
+ * Add named database table indexes.
+ *
+ * @author Matthias Kittsteiner
+ * @copyright 2023 KittMedia
+ * @license Free <https://shop.kittmedia.com/core/licenses/#licenseFree>
+ * @package com.kittmedia.wcf.visitstatistics
  */
-return [
-	PartialDatabaseTable::create('wcf1_visitor')
-		->columns([
-			NotNullVarchar255DatabaseTableColumn::create('browserName'),
-			IntDatabaseTableColumn::create('browserVersion')
-				->length(10),
-			NotNullVarchar255DatabaseTableColumn::create('osName'),
-			FloatDatabaseTableColumn::create('osVersion')
-		]),
-	DatabaseTable::create('wcf1_visitor_daily_system')
-		->columns([
-			ObjectIdDatabaseTableColumn::create('systemID'),
-			DateDatabaseTableColumn::create('date'),
-			NotNullVarchar255DatabaseTableColumn::create('browserName'),
-			IntDatabaseTableColumn::create('browserVersion')
-				->length(10),
-			NotNullVarchar255DatabaseTableColumn::create('osName'),
-			FloatDatabaseTableColumn::create('osVersion'),
-			IntDatabaseTableColumn::create('counter')
-				->length(10),
-			TinyintDatabaseTableColumn::create('isRegistered')
-				->defaultValue(0)
-				->length(1)
-				->notNull()
-		])
+
+use wcf\system\database\table\index\DatabaseTableIndex;
+use wcf\system\database\table\PartialDatabaseTable;
+
+$blueprint = [
+    PartialDatabaseTable::create('wcf1_visitor')
+        ->indices([
+            DatabaseTableIndex::create('')
+                ->columns(['time'])
+                ->type(DatabaseTableIndex::DEFAULT_TYPE)
+        ]),
+    PartialDatabaseTable::create('wcf1_visitor_daily')
+        ->indices([
+            DatabaseTableIndex::create('')
+                ->columns(['date', 'isRegistered'])
+                ->type(DatabaseTableIndex::UNIQUE_TYPE)
+        ]),
+    PartialDatabaseTable::create('wcf1_visitor_url')
+        ->indices([
+            DatabaseTableIndex::create('')
+                ->columns(['requestURI'])
+                ->type(DatabaseTableIndex::DEFAULT_TYPE)
+        ])
 ];
+$data = [];
+
+foreach ($blueprint as $blueprintTable) {
+    $data[] = PartialDatabaseTable::create($blueprintTable->getName())
+        ->indices(array_map(static function ($index) {
+            assert($index instanceof DatabaseTableIndex);
+
+            return DatabaseTableIndex::create($index->getName())
+                ->columns($index->getColumns())
+                ->type($index->getType())
+                ->drop();
+        }, $blueprintTable->getIndices()));
+}
+
+return $data;
